1706a697e37c9ab83c56613214745d761f57f108
Rafał Pruś <rafalprus@MacBook-Air-Rafa-2.local>
2023-06-30 19:45:48 +0200

    02

--- index.php
+++ index.php
@@ -3,15 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
+    <?php
+        $name = "Dark Matter";
+        $read = true;
+        if ($read) {
+            $message = "You have read $name";
+        } else {
+            $message = "You have'nt read $name";
+        }
 
+        $books = [
+                "Do Androids Dream of Electric Sheep",
+                "The Langoliers",
+                "Hail Mary"
+        ]
+    ?>
     <h1>
-        <?php
-            echo "Hello, World";
-
-        ?>
+        Recomended Books
     </h1>
+    <ul>
+        <?php foreach ($books as $book) {
+            echo "<li>$book</li>";
+        }
+        ?>
+    </ul>
 
 </body>
 </html>
