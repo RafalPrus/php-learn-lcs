43992e88ddec245ede99dbda00cea58b2df6d4d1
Rafał Pruś <rafalprues@gmail.com>
2023-07-02 22:27:19 +0200

    06

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
 
 require 'functions.php';
+require 'router.php';
 
-$uri = $_SERVER['REQUEST_URI'];
-
-if ($uri === '/') {
-    require 'controllers/index.php';
-} elseif ($uri === '/about') {
-    require 'controllers/about.php';
-} elseif ($uri === '/contact') {
-    require 'controllers/contact.php';
-}
