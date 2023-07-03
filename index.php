4006f872b11709d1020c92acedc677fe018be80b
Rafał Pruś <rafalprues@gmail.com>
2023-07-03 14:36:07 +0200

    07

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] ." </li>";
+};
+
+/*$second_pdo = new PDO($dsn);
+$second_statement = $second_pdo->prepare("INSERT INTO posts (title) VALUES('My Fourth Blog Post')");
+$second_statement->execute();*/
+
+
+
 
