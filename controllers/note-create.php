69788d7a70dae4e24947cf315bb5d9f1a2059c44
Rafał Pruś <rafalprues@gmail.com>
2023-07-04 22:54:55 +0200

    12

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,20 +1,21 @@
 <?php
 
-$config = require('config.php');
+require 'Validator.php';
+
+$config = require 'config.php';
 $db = new Database($config['database']);
 
 $heading = 'Create Note';
 
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $errors = [];
 
-    if (strlen($_POST['body']) === 0) {
-        $errors['body'] = 'Note can not be empty';
+
+    if (! Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'More than 1,000 characters not allowed.';
     }
 
-    if (strlen($_POST['body']) > 1000) {
-        $errors['body'] = 'Note is too long. No more than 1,000 characters.';
-    }
 
     if (empty($errors)) {
         $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
