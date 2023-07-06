5f3f3f9f6ca53cf81e29818f92cafd18f111171c
Rafał Pruś <rafalprues@gmail.com>
2023-07-06 18:57:18 +0200

    Middleware, redirect guest

--- routes.php
+++ routes.php
@@ -4,7 +4,7 @@ $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
-$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
 $router->get('/note', 'controllers/notes/show.php');
 $router->delete('/note', 'controllers/notes/destroy.php');
 $router->get('/notes/create', 'controllers/notes/create.view.php');
@@ -14,6 +14,6 @@ $router->get('/note/edit', 'controllers/notes/edit.php');
 
 $router->post('/notes', 'controllers/notes/store.php');
 
-$router->get('/register', 'controllers/registration/create.php');
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
 //$router->delete();
