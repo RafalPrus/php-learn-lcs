e097e26e22b1691e564d96c028e3023e54f8b81e
Rafał Pruś <rafalprues@gmail.com>
2023-07-07 12:12:35 +0200

    personalized welcoming, password hash

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -38,7 +38,7 @@ if ($user) {
 } else {
     $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
         'email' => $email,
-        'password' => $password,
+        'password' => password_hash($password, PASSWORD_BCRYPT),
     ]);
 
     // mark that the user has logged in
--- core/Router.php
+++ core/Router.php
@@ -74,3 +74,4 @@ class Router {
         die();
     }
 }
+
